57e94d8e3f546463319d35d002ed0f0e000108a2
JaehyunK <kk2007pp@gmail.com>
2025-03-03 19:05:41 +0900

    modify trading.py little

--- trading.py
+++ trading.py
@@ -13,20 +13,18 @@ class Trader:
 
         # Trader 클래스의 속성
         self.initial_balance = balance  # 초기 자본금
-        self.base_portfolio_value = balance
-        self.prev_portfolio_value = balance
         self.balance = balance  # 현재 현금 잔고
         self.prev_balance = balance 
         self.num_stocks = 0  # 보유 주식 수
+        # 포트폴리오 가치: balance + num_stocks * {현재 주식 가격} * (1-수수료)
+        self.portfolio_value = balance
+        self.prev_portfolio_value = balance
         self.memory = deque()
-        # 포트폴리오 가치: balance + num_stocks * {현재 주식 가격}
-        self.portfolio_value = 0
+        self.trading_unit = 0
         self.num_buy = 0  # 매수 횟수
         self.num_sell = 0  # 매도 횟수
         self.num_hold = 0  # 홀딩 횟수
-        self.immediate_reward = 0  # 즉시 보상
-        self.profitloss = 0  # 현재 손익
-        self.base_profitloss = 0  # 직전 지연 보상 이후 손익
+		self.curr_price = self.environment.curr_price()
         self.prev_price = self.environment.curr_price()
         self.pp_price = self.environment.curr_price()
         self.memory_earning_reward = []
@@ -35,15 +33,14 @@ class Trader:
     def reset(self):
         self.balance = self.initial_balance
         self.prev_portfolio_value = self.initial_balance
-        self.num_stocks = 0
         self.portfolio_value = self.initial_balance
+        self.num_stocks = 0
         self.num_buy = 0
         self.num_sell = 0
         self.num_hold = 0
-        self.immediate_reward = 0
-        self.ratio_portfolio_value = 0
         self.memory.clear()
         self.trading_unit = 0
+		self.curr_price = self.environment.curr_price()
         self.prev_price = self.environment.curr_price()
         self.pp_price = self.environment.curr_price()
 
@@ -72,15 +69,15 @@ class Trader:
         if action == parameters.ACTION_BUY:
             # 매수할 단위를 판단
             if recode: self.environment.set_buy_signal()
-            self.trading_unit = self.balance // curr_price
             self.num_buy += 1  # 매수 횟수 증가
-            self.balance -= curr_price * self.trading_unit  # 보유 현금을 갱신
+            self.trading_unit = self.balance // (curr_price * (1 + parameters.TRADING_TAX))
             self.num_stocks += self.trading_unit  # 보유 주식 수를 갱신
+            self.balance -= (curr_price * (1 + parameters.TRADING_TAX)) * self.trading_unit  # 보유 현금을 갱신
         # 매도
         elif action == parameters.ACTION_SELL:
             # 매도
             if recode: self.environment.set_sell_signal()
-            invest_amount = curr_price * self.trading_unit * (1- parameters.TRADING_TAX)
+            invest_amount = curr_price * self.trading_unit * (1 - parameters.TRADING_TAX)
             self.num_stocks -= self.trading_unit  # 보유 주식 수를 갱신
             self.balance += invest_amount  # 보유 현금을 갱신
             self.num_sell += 1  # 매도 횟수 증가
